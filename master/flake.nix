bd61e7fb4e1eede78298aa44004e10ca421819ca
hugosenari <hugosenari@gmail.com>
2022-03-22 00:36:30 -0300

    chore: re-index ../nimpkgs/p/pipe

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-pipe-master.flake = false;
   inputs.src-pipe-master.owner = "CosmicToast";
-  inputs.src-pipe-master.ref   = "refs/heads/master";
+  inputs.src-pipe-master.ref   = "master";
   inputs.src-pipe-master.repo  = "pipe";
   inputs.src-pipe-master.type  = "github";
   
--- v0_1_1/flake.nix
+++ v0_1_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-pipe-v0_1_1.flake = false;
   inputs.src-pipe-v0_1_1.owner = "CosmicToast";
-  inputs.src-pipe-v0_1_1.ref   = "refs/tags/v0.1.1";
+  inputs.src-pipe-v0_1_1.ref   = "v0_1_1";
   inputs.src-pipe-v0_1_1.repo  = "pipe";
   inputs.src-pipe-v0_1_1.type  = "github";
   
