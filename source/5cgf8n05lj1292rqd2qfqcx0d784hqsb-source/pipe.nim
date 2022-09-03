be8e8dd065327d8811b6a9b9f805571d80281853
hugosenari <hugosenari@gmail.com>
2022-09-03 19:50:47 -0300

    feat(nimpkgs): update lock

--- source/5cgf8n05lj1292rqd2qfqcx0d784hqsb-source/pipe.nim
+++
@@ -1,9 +0,0 @@
-import macros
-
-macro `|>`*(lhs, rhs: untyped): untyped =
-  case rhs.kind:
-  of nnkIdent: # single-parameter functions
-    result = newCall(rhs, lhs)
-  else:
-    result = rhs
-    result.insert(1, lhs)
